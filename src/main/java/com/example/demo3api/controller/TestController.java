a90098e7f15737bde634be51be4ab98aabe1ea1f
Francis Oyiogu <chigoziri.francis.oyiogu@cariad.technology>
2023-11-27 09:44:24 +0100

    ec2

---
+++ src/main/java/com/example/demo3api/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo3api.controller;
+
+import com.example.demo3api.payload.response.ApiResponse;
+import com.example.demo3api.payload.response.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("")
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping
+    public String health(){
+        return "API is running";
+    }
+}
